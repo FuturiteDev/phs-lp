bb374354895844c0d9fb771229edf07e848bab98
sxfuturite <sandra.gonzalez@futurite.com>
2026-05-29 10:20:02 -0600

    Ajuste rutas para vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+const isVercel = !!process.env.VERCEL;
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://phs-desarrollos.com',
-  base: '/naves-industriales',
+  base: isVercel ? '/' : '/naves-industriales',
   vite: {
     plugins: [tailwindcss()],
   },
